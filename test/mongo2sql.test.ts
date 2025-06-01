d7c2902655a51b5c51883a0a5b2086fde1d7cfa0
Jorge Rafael <jorge.rafael@simplisafe.com>
2025-06-01 09:44:34 -0300

    feat: add sql writer

---
+++ test/mongo2sql.test.ts
@@ -0,0 +1,22 @@
+import toSql from '../src/sql_writer';
+
+describe('Mongo to SQL Translation Tests', () => {
+    
+    it.each([
+        ["db.users.find({ });", "SELECT * FROM users;"],
+        ["db.users.find({ name: 'john doe', age: { $gt: 30 } });", "SELECT * FROM users WHERE name = 'john doe' AND age > 30;"],
+        ["db.products.find({ category: 'electronics' }, { name: 1, price: 1, _id: 1 });", "SELECT name, price, _id FROM products WHERE category = 'electronics';"],
+        ["db.orders.find({ totalAmount: { $lte: 100 } });", "SELECT * FROM orders WHERE totalAmount <= 100;"],
+        ["db.items.find({ status: { $ne: 'deleted' } });", "SELECT * FROM items WHERE status != 'deleted';"],
+        ["db.products.find({ category: { $in: ['electronics', 'books', 'assets'] } });", "SELECT * FROM products WHERE category IN ('electronics', 'books', 'assets');"],
+        ["db.users.find({ $and: [ { age: { $gt: 25 } }, { country: 'USA' } ] });", "SELECT * FROM users WHERE ((age > 25) AND (country = 'USA'));"],
+        ["db.orders.find({ $or: [ { status: 'pending' }, { amount: { $lt: 50 } } ] });", "SELECT * FROM orders WHERE ((status = 'pending') OR (amount < 50));"],
+        ["db.customers.find({ $and: [ { isActive: true }, { $or: [ { city: 'NY' }, { zip: '10001' } ] } ] });", "SELECT * FROM customers WHERE ((isActive = TRUE) AND (((city = 'NY') OR (zip = '10001'))));"]
+    ])('should translate a %s to %s', async (mongoQuery, sqlQuery) => {
+        
+        const sql = toSql(mongoQuery);
+        expect(sql).toBe(sqlQuery);
+    });
+
+
+});
