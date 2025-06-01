9cae51f60a5b57b9df4317b0173dd9097775de7b
Jorge Rafael <jorge.rafael@simplisafe.com>
2025-06-01 09:29:31 -0300

    feat: add more test

--- test/mongo_lexer.test.ts
+++ test/mongo_lexer.test.ts
@@ -45,6 +45,58 @@ describe('Mongo Lexer Tests', () => {
     expect(tokens).toEqual(expectedTokens);
   });
 
+   test('should tokenize with projection', () => {
+    const input = 'find({age: {$gte: 21}},{name: 1, _id: 1})';
+    const expectedTokens = [
+      { type: TokenType.IDENTIFIER, value: 'find' },
+      { type: TokenType.PUNCTUATOR, value: '(' },
+      { type: TokenType.PUNCTUATOR, value: '{' },
+      { type: TokenType.IDENTIFIER, value: 'age' },
+      { type: TokenType.PUNCTUATOR, value: ':' },
+      { type: TokenType.PUNCTUATOR, value: '{' },
+      { type: TokenType.OPERATOR, value: '$gte' },
+      { type: TokenType.PUNCTUATOR, value: ':' },
+      { type: TokenType.NUMBER, value: 21 },
+      { type: TokenType.PUNCTUATOR, value: '}' },
+      { type: TokenType.PUNCTUATOR, value: '}' },
+      { type: TokenType.PUNCTUATOR, value: ',' },
+      { type: TokenType.PUNCTUATOR, value: '{' },
+      { type: TokenType.IDENTIFIER, value: 'name' },
+      { type: TokenType.PUNCTUATOR, value: ':' },
+      { type: TokenType.NUMBER, value: 1 },
+      { type: TokenType.PUNCTUATOR, value: ',' },
+      { type: TokenType.IDENTIFIER, value: '_id' },
+      { type: TokenType.PUNCTUATOR, value: ':' },
+      { type: TokenType.NUMBER, value: 1 },
+      { type: TokenType.PUNCTUATOR, value: '}' },
+      { type: TokenType.PUNCTUATOR, value: ')' },  
+    ];
+    
+    const tokens = [...mongoLexer(input)];
+    expect(tokens).toEqual(expectedTokens);
+  });
+
+  test('should tokenize an array', () => {
+    const input = '{age: {$in: [21,22]} }'
+    const expectedTokens = [
+      { type: TokenType.PUNCTUATOR, value: '{' },
+      { type: TokenType.IDENTIFIER, value: 'age' },
+      { type: TokenType.PUNCTUATOR, value: ':' },
+      { type: TokenType.PUNCTUATOR, value: '{' },
+      { type: TokenType.OPERATOR, value: '$in' },
+      { type: TokenType.PUNCTUATOR, value: ':' },
+      { type: TokenType.PUNCTUATOR, value: '[' },
+      { type: TokenType.NUMBER, value: 21 },
+      { type: TokenType.PUNCTUATOR, value: ',' },
+      { type: TokenType.NUMBER, value: 22 },
+      { type: TokenType.PUNCTUATOR, value: ']' },
+      { type: TokenType.PUNCTUATOR, value: '}' },
+      { type: TokenType.PUNCTUATOR, value: '}' },  
+    ];
+    
+    const tokens = [...mongoLexer(input)];
+    expect(tokens).toEqual(expectedTokens);
+  });
 
 
 });
